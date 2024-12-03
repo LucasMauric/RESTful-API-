857f9b52d9ff2afca871bc9f003e1e7f92dbc91d
João Lucas <70124233+LucasMauric@users.noreply.github.com>
2024-12-02 21:16:30 -0300

    Adicionando a camada de dominio

--- src/main/java/me/dio/domain/model/BaseItem.java
+++ src/main/java/me/dio/domain/model/BaseItem.java
@@ -1,5 +1,6 @@
 package me.dio.domain.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public abstract class BaseItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/me/dio/domain/model/New.java
+++ src/main/java/me/dio/domain/model/New.java
@@ -2,7 +2,6 @@ package me.dio.domain.model;
 
 import jakarta.persistence.*;
 
-@Entity
-@Table(name = "tb_new")
+@Entity(name = "tb_new")
 public class New extends BaseItem{
 }
